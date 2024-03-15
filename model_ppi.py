4e9c88d9a47905f3444e9f77536cd93b487c9c22
HU YUE <75799499+huyue132@users.noreply.github.com>
2024-03-15 20:10:47 +0800

    Update model_ppi.py

--- model_ppi.py
+++ model_ppi.py
@@ -1,22 +1,29 @@
+import argparse
+from pathlib import Path
 
-import torch.nn as nn
-import torch.nn.functional as F
-from my_Utils import OUT_nodes
+path = Path("/home/new2/huyuecode/sdn2go/data")
+onto = 'MF'     # MF,CC,BP
+parser = argparse.ArgumentParser()
+orgs = 'human'
 
+parser.add_argument("--benchmark_list", type=str, default=path /f"data_{orgs}_{onto}_benchmark.csv")
+parser.add_argument("--data_sequence", type=str, default=path /f"data_{orgs}_sequences.csv")
+parser.add_argument("--anno_file", type=str, default=path / f"data_{orgs}_annotation.csv")
+parser.add_argument("--domain_file", type=str, default=path / f"data_{orgs}_domain.csv")
+parser.add_argument("--ppi_file", type=str, default=path / f"data_{orgs}_ppi_1024.csv")
 
-class PPI_Module(nn.Module):
-    def __init__(self, func):
-        super(PPI_Module, self).__init__()
-        self.ppi_conv1d = nn.Conv1d(1, 2, kernel_size=4).cuda()
-        self.ppi_hiddenlayer = nn.Linear(32762, 2048).cuda()
-        self.ppi_outlayer = nn.Linear(2048, OUT_nodes[func]).cuda()
+parser.add_argument("--esm_dir", type=str, default=path /f"{orgs}_esm_dir")
 
-    def forward(self, ppiVec):
-        ppi_out = self.ppi_conv1d(ppiVec.unsqueeze(1))
-        ppi_out = F.relu(ppi_out)
-        ppi_out = ppi_out.view(ppi_out.size(0), -1)
-        ppi_out = F.dropout(self.ppi_hiddenlayer(ppi_out), p=0.3, training=self.training)
-        ppi_out = F.relu(ppi_out)
-        ppi_out = self.ppi_outlayer(ppi_out)
-        ppi_out = F.sigmoid(ppi_out)
-        return ppi_out
+parser.add_argument("--device", type=str, default="cuda")
+parser.add_argument("--onto", type=str, default=onto)
+parser.add_argument("--orgs", type=str, default=orgs)
+parser.add_argument("--max_seq_len", type=int, default=1000, help="max len of protein sequence")
+parser.add_argument("--min_seq_len", type=int, default=16, help="min len of protein sequence")
+if orgs=='human':
+    parser.add_argument("--max_domain_len", type=int, default=357, help="min len of human protein domain")
+else:
+    parser.add_argument("--max_domain_len", type=int, default=40, help="min len of yeast protein domain")
+
+def get_config():
+    args = parser.parse_args()
+    return args
